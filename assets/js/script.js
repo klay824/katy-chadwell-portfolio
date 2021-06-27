b8368108b9c7d480448f220f532cec60fc72eb9a
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-27 15:17:48 -0500

    moved assets folder, deleted unneeded boiler plate react files

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-function updateTime(){
-    let currentTime = moment().format('h:mm a');
-    $(".time").text('It is ' + currentTime + '. ' + ' Do you know where your next full-stack web developer is?');
-}
-
-updateTime();
-setInterval(function() {
-    updateTime();
-},60000);
