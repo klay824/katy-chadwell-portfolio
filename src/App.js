72f6da5b001c1c753141811bf050441b2787a94c
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-27 15:15:23 -0500

    imported HashRouter added route for About section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import About from "./Components/Pages/About";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/about" component={About} />
+      </div>
+    </Router>
   );
 }
 
