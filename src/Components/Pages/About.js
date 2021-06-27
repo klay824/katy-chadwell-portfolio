1b5d8bf5e52ddf662e8b12a0b37b9a23990d5d14
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-27 15:16:35 -0500

    created Components folder and Header.js and Footer.js

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../../../src/style.css"
+
+function About() {
+    return (
+        <div className="container mt-5">
+            <div className="col-s-12 col col-lg-6 col-xl-6 pl-5">
+                <img className="photo-of-me img-fluid" alt="me" src="assets\images\photo-of-me.jpg"></img>
+            </div>
+            <div className="col-s-12 col-lg-6 col-xl-6 pl-5">
+                <h2>About Me</h2>
+                <p className="flow-text mb-5 my-section">
+                    I am a full-stack web developer based in the Dallas/Fort Worth Metroplex and I would love to be your next web developer. I am skilled in HTML, CSS, Bootstrap, JavaScript, JQuery, server-side APIs, Node.js, Express.js, MySQL, Sequelize, MongoDB, and Mongoose ODM. I enjoy making dynamic, creative websites that react to the user.
+                    <br></br>
+                    <br></br>
+                    I currently live in Terrell, TX with my husband Austin, dog Chuck, and cats Jade and Talon. I look forward to speaking with you soon!
+                </p>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default About;
