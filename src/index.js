0d210f893304fcfc7ea54f53fea9b7e1809a3b11
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-27 22:00:00 -0500

    imported in Carousel

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from "react-bootstrap";
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
