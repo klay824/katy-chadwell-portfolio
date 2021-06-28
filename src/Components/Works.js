51cd006ec153d0cc6fef318991a7984ecd28799a
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-27 22:00:59 -0500

    wrote Works.js and rendered WorksDetail

---
+++ src/Components/Works.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import WorksDetail from "../Components/Pages/WorksDetail";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="mt-5">
+                <WorksDetail />
+            </div>
+        )
+    }
+}
+
+export default App;
