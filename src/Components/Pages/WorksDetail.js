8d43d0dd8b996957eeaf112a9efa7465c6ddcb9c
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-29 13:04:26 -0500

    got carousel text and footer to be responsive

--- src/Components/Pages/WorksDetail.js
+++ src/Components/Pages/WorksDetail.js
@@ -9,18 +9,16 @@ function WorksDetail() {
     return (
         <div className="container pt-5">
             <div className="row justify-content-center">
-                <h4 className="col-6">Check Out Some of My Work</h4>
                 <Carousel className="col-12">
                     {works.map(work => (
                         <>
                             <div>
                                 <img src={work.img} alt={work.alt} />
                                 <div className="slide-text">
-                                    <h4>{work.title}</h4>
+                                    <p>{work.title}</p>
                                     <a href={work.deployed}>View Deployed</a><a href={work.repo}>View Repository</a>
-                                    <h5>{work.description}</h5>
+                                    <p>{work.description}</p>
                                 </div>
-
                             </div>
                         </>
                     ))}
