103d5d082ce6ae0ebf694eda739a903a1ca825bc
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-28 22:29:38 -0500

    added Route path for the home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Header from "./Components/Header";
+import Jumbotron from "./Components/Pages/Jumbotron";
 import About from "./Components/Pages/About";
 import Works from "./Components/Works";
 import Footer from "./Components/Footer";
 import "./style.css";
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
 
         <Header />
         <main>
+          <Route exact path="/" component={Jumbotron} />
           <Route exact path="/about" component={About} />
           <Route exact path="/work" component={Works} />
 
