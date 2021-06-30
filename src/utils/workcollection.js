1853aadefdcfb00edaadfc4506e9ebbd2d699f6f
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-30 17:07:52 -0500

    created new js file to hold my json data in order to import images instead of linking to their paths

---
+++ src/utils/workcollection.js
@@ -0,0 +1,75 @@
+import ron from "../assets/images/rons-grub-hub.png";
+import emp from "../assets/images/employee-directory.png";
+import budget from "../assets/images/budget-tracker.png";
+import fitness from "../assets/images/fitness-tracker-1.png";
+import tech from "../assets/images/tech-blog.png";
+import movie from "../assets/images/ultimate-movie.png";
+import note from "../assets/images/note-taker.png";
+
+const works = [
+    {
+        "id": 1,
+        "title": "Ron's Grub Hub",
+        "description": "A fun restaurant locator based off the character Ron Swanson from Parks & Recreation.",
+        "repo": "https://github.com/klay824/project-1",
+        "deployed": "https://klay824.github.io/project-1/",
+        "img": ron,
+        "alt": "Ron's Grub Hub"
+    },
+    {
+        "id": 2,
+        "title": "Employee Directory",
+        "description": "A React app that allows users to view, search, and filter their employees.",
+        "repo": "https://github.com/klay824/employee-directory",
+        "deployed": "https://klay824.github.io/employee-directory/",
+        "img": emp,
+        "alt": "Employee Directory"
+    },
+    {
+        "id": 3,
+        "title": "Budget Tracker",
+        "description": "A full stack application that allows users to enter deposits and expenses both online and offline.",
+        "repo": "https://github.com/klay824/budget-tracker",
+        "deployed": "https://enigmatic-citadel-00070.herokuapp.com/",
+        "img": budget,
+        "alt": "Budget Tracker"
+    },
+    {
+        "id": 4,
+        "title": "Fitness Tracker",
+        "description": "A full-stack blogging application that allows the user to create, edit, and delete blog posts, as well as comment on posts.",
+        "repo": "https://github.com/klay824/fitness-tracker",
+        "deployed": "https://aqueous-forest-70014.herokuapp.com/",
+        "img": fitness,
+        "alt": "Fitness Tracker"
+    },
+    {
+        "id": 5,
+        "title": "Tech Blog",
+        "description": "A full-stack blogging application that allows the user to create, edit, and delete blog posts, as well as comment on posts.",
+        "repo": "https://github.com/klay824/tech-blog",
+        "deployed": "https://intense-bayou-51128.herokuapp.com/",
+        "img": tech,
+        "alt": "Tech Blog"
+    },
+    {
+        "id": 6,
+        "title": "The Ultimate Movie List",
+        "description": "A personal movie database that allows authenticated users store a list of movies they want to watch to make a quick selection on movie night.",
+        "repo": "https://github.com/klay824/the-ultimate-movie-list",
+        "deployed": "https://glacial-hollows-98883.herokuapp.com/",
+        "img": movie,
+        "alt": "Ultimate Movie List"
+    },
+    {
+        "id": 7,
+        "title": "Note-Taker App",
+        "description": "A note-taking app that allows the user to create a list of notes for themselves throughout the day.",
+        "repo": "https://github.com/klay824/note-taker",
+        "deployed": "https://klay824-note-taker.herokuapp.com/",
+        "img": note,
+        "alt": "Note-Take App"
+    }
+]
+
+export default works;
